5e5b833b8f1c94d9f3caa68619a0c5efd245f4a3
saramccombs <theSaraMcCombs@gmail.com>
2019-12-01 19:25:19 -0500

    updated pages

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,9 +4,53 @@ class TweetsController < ApplicationController
   get '/tweets' do
     if logged_in?
       @tweets = Tweet.all
+      @users = User.all
       erb :'/tweets/tweets'
     else
       redirect to '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect to "/login"
+    end
+  end
+
+  post '/tweets' do
+    if logged_in?
+      @tweet = Tweet.new(content: params[:content], user_id: current_user.id)
+      if @tweet.content == ""
+      redirect to "/tweets/new"
+      else
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
+      end
+    else
+      redirect to "/login"
+    end
+    
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @users = User.all
+      @tweet = Tweet.find(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect to "/login"
+    end
+    
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'/tweets/edit_tweet'
+    else
+      redirect to "/login"
     end
   end
 end
